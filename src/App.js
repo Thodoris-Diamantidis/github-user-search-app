265c6ace528156817494e6b5fd5d7bf49de37ed1
Thodoris-Diamantidis <thodoris_diamantidis@hotmail.com>
2023-10-21 15:25:20 +0300

    finished with mobile design added a bit of tablet/pc

--- src/App.js
+++ src/App.js
@@ -1,16 +1,78 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
+import profile from "./assets/Oval.png";
+import { ReactComponent as Location } from "./assets/icon-location.svg";
+import { ReactComponent as Twitter } from "./assets/icon-twitter.svg";
+import { ReactComponent as Website } from "./assets/icon-website.svg";
+import { ReactComponent as Company } from "./assets/icon-company.svg";
+import { ReactComponent as Sun } from "./assets/icon-sun.svg";
 
 export default function App() {
   return (
     <main>
       <article className="github">
-        <div className="github_heading">
-          <p>devfinder</p>
-          <button>Light</button>
+        <div className="github__heading">
+          <h1>devfinder</h1>
+          <button className="btn__theme">
+            Dark <Sun />
+          </button>
         </div>
         <SearchBar />
-        <div>API RESULTS</div>
+        <div className="github__details">
+          <div className="github__user">
+            <div>
+              <img className="github__profile" src={profile} />
+            </div>
+            <div>
+              <p>The Octocat</p>
+              <h3>@Octocat</h3>
+              <h3>Joined 25,Jan 2011</h3>
+            </div>
+          </div>
+
+          <div className="github__status">
+            <h3 className="align-left">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+              odio. Quisque volutpat mattis eros.
+            </h3>
+          </div>
+
+          <div className="github__stats">
+            <p>
+              <span>Repos</span> <span>8</span>
+            </p>
+
+            <p>
+              <span>Followers</span> <span>3938</span>
+            </p>
+
+            <p>
+              <span>Following</span> <span>9</span>
+            </p>
+          </div>
+
+          <div className="github__links">
+            <div className="github__link">
+              <Location fill="white" />
+              San Francisco
+            </div>
+
+            <div className="github__link">
+              <Twitter fill="white" />
+              <a href="">Twitter</a>
+            </div>
+
+            <div className="github__link">
+              <Website fill="white" />
+              <a href="">https://github.blog</a>
+            </div>
+
+            <div className="github__link">
+              <Company fill="white" />
+              <a href="">@github</a>
+            </div>
+          </div>
+        </div>
       </article>
     </main>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchBar() {
-  return <div>SearchBar</div>;
+  const [searchInput, setSearchInput] = useState("");
+
+  return (
+    <div className="github__searchbar">
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Search GitHub username..."
+          value={searchInput}
+          onChange={(event) => {
+            event.preventDefault();
+            setSearchInput(event.target.value);
+          }}
+        />
+        <button>Search</button>
+      </div>
+    </div>
+  );
 }
